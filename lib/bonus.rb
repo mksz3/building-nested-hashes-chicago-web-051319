fdecf58dcd845a856bd7aa3acc3628395ef8b7fb
Markus Sanders <markus.sanderst@gmail.com>
2019-05-02 19:58:54 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,7 @@ def bonus
    }
   }
 
-  status = 'dead'
+ [:montague][:hero][:status] = 'dead'
   
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
